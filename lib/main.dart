7edc7c50f9d9d043d84eb4465fdc4ebb259d211a
serena <small08261995@gmail.com>
2023-03-06 19:50:11 +0800

    configure the theme and style

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellowAccent),
         ),
         home: MyHomePage(),
       ),
@@ -39,21 +39,54 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESONE idea:'),
-          Text(appState.current.asLowerCase),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            // just takes space, commonly used to create visual gaps
+            SizedBox(height: 10),
+            // button
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              }, 
+            child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-          // button
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            }, 
-          child: Text('Next'),
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  // theme and style
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase, style: style,
+          semanticsLabel: pair.asPascalCase,
           ),
-        ],
       ),
     );
   }
